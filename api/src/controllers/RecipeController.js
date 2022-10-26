fbc1258a6f26b92cdd28118eefe2b85fefb602f0
Francisco Giudicatti <franciscogiudicatti11@gmail.com>
2022-10-26 12:09:51 -0300

    add some changes front and back

--- api/src/controllers/RecipeController.js
+++ api/src/controllers/RecipeController.js
@@ -2,13 +2,13 @@ require("dotenv").config();
 const axios = require("axios");
 const { Recipe, Diet } = require("./../db.js");
 const API_URL = "https://api.spoonacular.com/recipes/";
-const { API_KEY, API_KEY2, API_KEY3, API_KEY4, API_KEY5, API_KEY6, API_KEY7, API_KEY8, API_KEY9 } = process.env;
+const { API_KEY, API_KEY2, API_KEY3, API_KEY4, API_KEY5, API_KEY6, API_KEY7, API_KEY8, API_KEY9, API_KEY10, API_KEY11 } = process.env;
 
 
 const getApiRecipes = async () => {
   try {
     const apiRecipes = await axios.get(
-      `${API_URL}complexSearch?apiKey=${API_KEY7}&addRecipeInformation=true&number=100`
+      `${API_URL}complexSearch?apiKey=${API_KEY2}&addRecipeInformation=true&number=100`
     );
     const formatApiRecipes = apiRecipes.data.results.map((recipe) => {
       return {
@@ -26,7 +26,7 @@ const getApiRecipes = async () => {
     return formatApiRecipes;
     
   } catch (error) {
-    console.log('La pegada a la API no respondio..', error)
+    console.log('Something with API goes wrong..', error)
   }
 };
 
@@ -44,7 +44,7 @@ const getDbRecipes = async () => {
 
     return dbRecipes
   } catch (error) {
-    console.log('La union con las dos tablas salio mal', error)
+    console.log('Something with UNION between tables goes wrong..', error)
   }
 };
 
@@ -55,7 +55,7 @@ const getAllRecipes = async () => {
     const dbRecipes = await getDbRecipes();
     return [...apiRecipes, ...dbRecipes];
   } catch (error) {
-    console.log('La concatenación con la info de la API y mi DB salio mal..', error)
+    console.log('Something with UNION of the record between API and DB goes wrong..', error)
   }
 };
 
@@ -80,7 +80,7 @@ const getAllRecipesOrFilterByName = async (req,res,next) => {
     const allRecipes = await getAllRecipes();
     if(name){
       const filterByName = allRecipes.filter(recipe => recipe.name.toLowerCase().includes(name.toLowerCase().trim()));
-      return filterByName.length ? res.status(200).json(filterByName) : res.status(404).json({msg: 'Recipe not matched with the name sended.'})
+      return !filterByName.length ? res.status(404).json({msg: 'Recipe not matched with the name sended.'}) : res.status(200).json(filterByName);
     }
     res.status(200).json(allRecipes)
   } catch (error) {
@@ -91,20 +91,22 @@ const getAllRecipesOrFilterByName = async (req,res,next) => {
 const postRecipe = async(req,res,next) => {
   try {
     const { name, summary, steps, diets, healthScore, image } = req.body;
-    if(!name || !summary || !diets) res.status(400).json({msg: 'Should be send the name, diets and summary.'})
-    const addRecipe = await Recipe.create({ name, summary, steps, healthScore, image: image || 'https://i.ibb.co/Ykth1KM/icono-1-1.png' });
-    diets.forEach(async (d) => {
-      const diet = await Diet.findOne({
-        where: { name: d},
-      })
-      addRecipe.addDiet(diet);
-    })
-    // const findDiets = await Diet.findAll({ where: {
-    //   name: diets
-    // }})
-
-    // await addRecipe.addDiet(findDiets)
-    return res.status(201).json(addRecipe) //no seria mejor mandar el obj creado? {msg: 'Recipe has been created!'}
+    if(!name || !summary || !Array.isArray(diets) || diets.length === 0) return res.status(400).json({msg: 'Should be send the name, diets and summary.'})
+    // diets.forEach(async (d) => {
+    //   const diet = await Diet.findOne({
+    //     where: { name: d},
+    //   })
+    //   addRecipe.addDiet(diet);
+    // })
+    const findDiets = await Diet.findAll({ where: {
+      name: diets
+    }})
+    if(findDiets.length === diets.length){
+      const addRecipe = await Recipe.create({ name, summary, steps, healthScore, image: image || 'https://i.ibb.co/Ykth1KM/icono-1-1.png' });
+      await addRecipe.addDiet(findDiets);
+      return res.status(201).json(addRecipe) //no seria mejor mandar el obj creado? {msg: 'Recipe has been created!'}
+    }
+    res.status(400).json({msg: 'Please enter a valid diet.'});
   } catch (error) {
     next(error)
   }
@@ -120,7 +122,7 @@ const deleteRecipe = async(req,res,next) => {
       where: { id }
     })
 
-    res.status(200).json(id)
+    res.status(200).json({msg: 'Recipe has been removed!'})
   } catch (error) {
     next(error)
   }
